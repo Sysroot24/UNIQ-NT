7d63076002a76efe6891e6e0ce429f065d4e8123
ANKIT THAKUR <107117919+Sysroot24@users.noreply.github.com>
2024-12-26 14:50:37 +0530

    Delete java.js

--- java.js
+++
@@ -1,9 +0,0 @@
-// script.js
-function toggleMenu() {
-    const menu = document.querySelector('.menu-items');
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
-}
